8921c0025dc53c5d12bf990a38a859cc5b7caf9a
Adrianma13 <adrianmachucamorilla@gmail.com>
2026-03-18 19:46:38 +0100

    test: Añadimos un test erroneo

--- src/test/java/com/mgcss/mgcss_track_7/ApplicationTests.java
+++ src/test/java/com/mgcss/mgcss_track_7/ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.mgcss.mgcss_track_7;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1,2);
 	}
 
 }
